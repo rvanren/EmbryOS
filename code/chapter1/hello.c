97db07bb60083bfd8f9df7d891f902bd5582e1f1
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-17 03:17:39 -0400

    Fixed some small issues

--- code/chapter1/hello.c
+++ code/chapter1/hello.c
@@ -1,17 +1,17 @@
-typedef unsigned int uint32_t
+typedef unsigned int uint32_t;
 
 struct uart {
     uint32_t txdata;
-}
+};
 
 // Memory mapped IO:
 #define UART ((struct uart *) 0x10010000)
 #define TXFULL (1 << 31)
 
 void putchar(char c) {
-    while (UART_TXDATA & TXFULL)
+    while (UART->txdata & TXFULL)
         ;
-    UART_TXDATA = c;
+    UART->txdata = c;
 }
 
 void printf(char *str) {
