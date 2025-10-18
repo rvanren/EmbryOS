8923ad2b65557959a842125268d66d152c2158a8
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-18 04:41:47 -0400

    Working more or less

--- code/chapter4/hello.c
+++ code/chapter4/hello.c
@@ -1,15 +1,16 @@
-#include "frame.h"
-#include "stdio.h"
-#include "process.h"
-#include "clint.h"
-#include "ctx.h"
-#include "mtime.h"
+#include "frame.h"                                                              #include "stdio.h"                                                              #include "process.h"                                                            #include "clint.h"
+#include "ctx.h"                                                                #include "mtime.h"
 
 #define QUANTUM          100000         // 100 milliseconds
 #define N_PRIORITIES     3
 
 int run_queue[N_PRIORITIES];
 int proc_current;       // current priority
+
+void panic(char *s) {
+    printf("PANIC %s\n", s);
+    for (;;) ;
+}
 
 // The current process yields to the next process to run.
 static void yield() {
@@ -27,12 +28,20 @@ static void yield() {
         }
         else {
             former->next = current->next;
+        if (former->next < 0) panic("1?\n");
         }
 
         // Insert into next priority
+    if (run_queue[1] < 0) {
+        current->next = pid;
+    }
+    else {
         former = FRAME(struct pcb, run_queue[1]);
         current->next = former->next;
+        if (current->next < 0) panic("2?\n");
         former->next = pid;
+        if (former->next < 0) panic("3?\n");
+    }
         run_queue[1] = pid;
     }
     else {
@@ -40,12 +49,15 @@ static void yield() {
     }
 
     // Find the next priority to run
-    int p = 0;
-    while (p < N_PRIORITIES)
-        if (run_queue[p] >= 0) break;
+    proc_current = 0;
+    while (proc_current < N_PRIORITIES) {
+        if (run_queue[proc_current] >= 0) break;
+    proc_current++;
+    }
 
     // Switch contexts
-    former = FRAME(struct pcb, run_queue[p]);
+    former = FRAME(struct pcb, run_queue[proc_current]);
+    // printf("SWITCH %d %d %d\n", proc_current, run_queue[proc_current], former->next);
     struct pcb *next = FRAME(struct pcb, former->next);
     ctx_switch(&current->sp, next->sp);
 }
@@ -58,12 +70,14 @@ void timer_handler() {
 static void delay(void) {
     interrupts_enable();
     for (volatile int i = 0; i < 100000; i++)
-        yield();
+        ;
     interrupts_disable();
 }
 
 int current_pid() {
     struct pcb *former = FRAME(struct pcb, run_queue[proc_current]);
+    printf("CP %d %d %d\n", proc_current, run_queue[proc_current], former->next);
+    if (former->next < 0) panic("CP\n");
     return former->next;
 }
 
@@ -71,7 +85,7 @@ void taskA(void) {
     struct pcb *self = FRAME(struct pcb, current_pid());
     for (int cnt = 0;; cnt++) {
         proc_put(self, 0, 0, 'A', 2, 0);
-        printf(" %x %d", self, cnt);
+        printf("TA %x %d", self, cnt);
         delay();
     }
 }
@@ -81,11 +95,14 @@ static void make_runnable(int pid, int priority) {
     if (run_queue[priority] < 0) {
         run_queue[priority] = pid;
         next->next = pid;
+        if (next->next < 0) panic("4?\n");
     }
     else {
         struct pcb *former = FRAME(struct pcb, run_queue[priority]);
         next->next = former->next;
+        if (next->next < 0) panic("5?\n");
         former->next = pid;
+        if (former->next < 0) panic("9?\n");
     }
 }
 
@@ -93,6 +110,8 @@ void run(int pid, entry_t fn) {
     struct pcb *former = FRAME(struct pcb, run_queue[proc_current]);
     struct pcb *current = FRAME(struct pcb, former->next);
     make_runnable(pid, 0);
+    proc_current = 0;
+    printf("RUN %d %d\n", proc_current, run_queue[proc_current]);
     ctx_start(&current->sp, &frames[pid].bytes[PAGE_SIZE], fn);
 }
 
@@ -104,6 +123,7 @@ void make_unrunnable() {
     }
     else {
         former->next = current->next;
+        if (former->next < 0) panic("6?\n");
     }
 }
 
@@ -114,8 +134,8 @@ int main(void) {
     clint_init();
     clint_set_handler(CLINT_TIMER, timer_handler);
 
-    // mtime_init();
-    // mtime_reset(QUANTUM);
+    mtime_init();
+    mtime_reset(QUANTUM);
 
     // Allocate processes
     int ul = proc_create((struct rect){ 0,   0,  40, 12 });  // upper-left
@@ -130,8 +150,11 @@ int main(void) {
     run(lr, taskA);
 
     // Switch priority to level 2
+    int pid = current_pid();
     make_unrunnable();
-    make_runnable(current_pid(), 2);
+    make_runnable(pid, 2);
+    proc_current = 2;
+
     yield();
 
     // Run the main loop, which is waiting for interrupts
