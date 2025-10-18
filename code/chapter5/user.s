d9e178a5feaf23c3cc22c3b1fe88d525c0d618a8
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-18 13:04:45 -0400

    Added missing code

--- code/chapter5/user.s
+++ code/chapter5/user.s
@@ -4,6 +4,21 @@
     .section .text
     .globl ctx_user
 ctx_user:
+    addi sp, sp, -64
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
+    sw s7, 32(sp)
+    sw s8, 36(sp)
+    sw s9, 40(sp)
+    sw s10, 44(sp)
+    sw s11, 48(sp)
+    sw ra, 52(sp)
+
     # a0 = &old_sp, a1 = new_sp, a2 = entry
     sw  sp, 0(a0)           # save current sp
     mv  sp, a1              # switch to new stack
