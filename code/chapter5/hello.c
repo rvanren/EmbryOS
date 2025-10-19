6d5a2783a426ecd9289a7be9d26c2e247762611f
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-19 09:19:38 -0400

    Latest

--- code/chapter5/hello.c
+++ code/chapter5/hello.c
@@ -31,7 +31,6 @@ int main(void) {
     intr_set_handler(INTR_SYSCALL, syscall_handler);
     intr_set_handler(INTR_EXTERNAL, interrupt_handler);
     mtime_reset(QUANTUM);
-    ctx_user_setup();
 
     sched_run(taskA, (struct rect){  0,  0, 40, 12 });  // upper-left
     sched_run(taskB, (struct rect){ 40,  0, 40, 12 });  // upper-right
