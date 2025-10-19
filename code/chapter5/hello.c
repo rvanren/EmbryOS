d550518e108dc9106d7e697159631fcc1c41d4cf
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-19 09:22:04 -0400

    Latest

--- code/chapter5/hello.c
+++ code/chapter5/hello.c
@@ -29,12 +29,11 @@ int main(void) {
     sched_init(pcb);
     intr_set_handler(INTR_TIMER, timer_handler);
     intr_set_handler(INTR_SYSCALL, syscall_handler);
-    intr_set_handler(INTR_EXTERNAL, interrupt_handler);
     mtime_reset(QUANTUM);
 
     sched_run(taskA, (struct rect){  0,  0, 40, 12 });  // upper-left
-    sched_run(taskB, (struct rect){ 40,  0, 40, 12 });  // upper-right
-    sched_run(taskB, (struct rect){  0, 12, 40, 12 });  // lower-left
+    sched_run(taskA, (struct rect){ 40,  0, 40, 12 });  // upper-right
+    sched_run(taskA, (struct rect){  0, 12, 40, 12 });  // lower-left
     sched_run(taskA, (struct rect){ 40, 12, 40, 12 });  // lower-left
 
     sched_idle();
