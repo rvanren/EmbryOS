3807d27cf844b92316f8baea93fbfb5799233790
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-18 21:29:19 -0400

    Clarification

--- code/chapter6/trap.h
+++ code/chapter6/trap.h
@@ -3,19 +3,16 @@
 
 #include <stdint.h>
 
-#define TRAP_FRAME_SIZE 128
+#define TRAP_FRAME_SIZE 128                 // 32 4-byte registers
 
 struct trap_frame {
-    uint32_t ra;
-    uint32_t sp;
-    uint32_t gp;
-    uint32_t tp;
-    uint32_t t0, t1, t2;
-    uint32_t s0, s1;
-    uint32_t a0, a1, a2, a3, a4, a5, a6, a7;
-    uint32_t s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
-    uint32_t t3, t4, t5, t6;
-    uint32_t mepc;
+    uint32_t ra, sp, gp, tp;                // special purpose registers
+    uint32_t t0, t1, t2;                    // caller-saved temps
+    uint32_t s0, s1;                        // callee-saved
+    uint32_t a0, a1, a2, a3, a4, a5, a6, a7;    // syscall arguments & results
+    uint32_t s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;  // callee-saved
+    uint32_t t3, t4, t5, t6;                // caller-saved
+    uint32_t mepc;                          // saved program counter
 };
 
 #endif
