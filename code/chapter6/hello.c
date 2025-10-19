ca465932f47351a62ddee2ecea38d44aecdc0e56
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-18 20:49:41 -0400

    Latest

--- code/chapter6/hello.c
+++ code/chapter6/hello.c
@@ -7,7 +7,6 @@
 #include "mtime.h"
 #include "syscall.h"
 #include "syslib.h"
-#include "uart.h"
 
 #define QUANTUM          50000         // 50 milliseconds
 
@@ -24,32 +23,25 @@ void taskA(void) {
 }
 
 void taskB(void) {
-    int counter = 0;
-    for (;;) {
+    for (int counter = 0;;, counter++) {
         char c = user_get();
         user_put(10, 5 + counter % 10, c, 2, 0);
-        counter++;
     }
 }
 
 int main(void) {
-    frame_init();
+    frame_init(); intr_init(); syscall_init(); uart_init(); mtime_init();
     struct pcb *pcb = proc_init((struct rect){ 0, 0, 80, 24 });
     sched_init(pcb);
-    intr_init();
     intr_set_handler(INTR_TIMER, timer_handler);
-    syscall_init();
     intr_set_handler(INTR_SYSCALL, syscall_handler);
-    uart_init();
     intr_set_handler(INTR_EXTERNAL, interrupt_handler);
-    mtime_init();
     mtime_reset(QUANTUM);
     ctx_user_setup();
 
     sched_run(taskA, (struct rect){ 0,   0,  40, 12 });  // upper-left
     sched_run(taskB, (struct rect){ 40,  0,  40, 12 });  // upper-right
     sched_run(taskA, (struct rect){ 0,  12,  40, 12 });  // lower-left
-    sched_run(taskA, (struct rect){ 40, 12,  40, 12 });  // lower-right
 
     // Switch priority to level 2
     proc_dequeue(&run_queue[proc_current]);
