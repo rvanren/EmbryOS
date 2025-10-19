c9660b59c211d8aa7082a46bb4d995a0b4e2588b
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-19 09:13:10 -0400

    Working on Chapter 5

--- code/chapter5/hello.c
+++ code/chapter5/hello.c
@@ -8,7 +8,6 @@
 #include "syscall.h"
 #include "syslib.h"
 #include "uart.h"
-#include "plic.h"
 
 #define QUANTUM          50000         // 50 milliseconds
 
@@ -24,13 +23,6 @@ void taskA(void) {
     }
 }
 
-void taskB(void) {
-    for (int counter = 0;; counter++) {
-        char c = user_get();
-        user_put(10, 5 + counter % 10, c, 2, 0);
-    }
-}
-
 int main(void) {
     frame_init(); intr_init(); syscall_init(); uart_init(); mtime_init();
     struct pcb *pcb = proc_init((struct rect){ 0, 0, 80, 24 });
@@ -41,9 +33,10 @@ int main(void) {
     mtime_reset(QUANTUM);
     ctx_user_setup();
 
-    sched_run(taskA, (struct rect){ 0,   0,  40, 12 });  // upper-left
-    sched_run(taskB, (struct rect){ 40,  0,  40, 12 });  // upper-right
-    sched_run(taskA, (struct rect){ 0,  12,  40, 12 });  // lower-left
+    sched_run(taskA, (struct rect){  0,  0, 40, 12 });  // upper-left
+    sched_run(taskB, (struct rect){ 40,  0, 40, 12 });  // upper-right
+    sched_run(taskB, (struct rect){  0, 12, 40, 12 });  // lower-left
+    sched_run(taskA, (struct rect){ 40, 12, 40, 12 });  // lower-left
 
     sched_idle();
 }
