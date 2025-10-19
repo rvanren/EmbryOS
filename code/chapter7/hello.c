fd997a642e352dd5e0bd9197123a3510ab29abd2
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-19 18:14:57 -0400

    Working on virtual memory

--- code/chapter7/hello.c
+++ code/chapter7/hello.c
@@ -1,16 +1,8 @@
-#include "frame.h"
-#include "sched.h"
-#include "stdio.h"
-#include "trap.h"
-#include "interrupt.h"
-#include "ctx.h"
-#include "mtime.h"
-#include "syscall.h"
-#include "syslib.h"
+#include "frame.h"                                                              #include "sched.h"                                                              #include "stdio.h"                                                              #include "trap.h"                                                               #include "interrupt.h"                                                          #include "ctx.h"                                                                #include "mtime.h"                                                              #include "syscall.h"                                                            #include "syslib.h"
 #include "uart.h"
 #include "plic.h"
 
-#define QUANTUM          50000         // 50 milliseconds
+#define QUANTUM          5000000         // 50 milliseconds
 
 void timer_handler(struct trap_frame *tf) {
     sched_yield();
@@ -18,32 +10,29 @@ void timer_handler(struct trap_frame *tf) {
 }
 
 void taskA(void) {
-    for (int cnt = 0;; cnt++) {
-        user_put(2 + cnt % 3, 2 + cnt % 3, 'A' + cnt % 26, cnt, 7 - cnt % 8);
-        for (volatile int i = 0; i < 100000; i++) ;
-    }
-}
-
-void taskB(void) {
-    for (int counter = 0;; counter++) {
-        char c = user_get();
-        user_put(10, 5 + counter % 10, c, 2, 0);
-    }
+    struct pcb *self = run_queue[proc_current]->next;
+    void user_load(struct pcb *p);
+    void user_run(struct pcb *p);
+    self->pagetable = vm_root();
+    printf("LOAD\n");
+    user_load(self);
+    printf("RUN\n");
+    user_run(self);
+    printf("DONE\n");
 }
 
 int main(void) {
     frame_init(); intr_init(); plic_init(); syscall_init(); uart_init(); mtime_init();
+    vm_init(); vm_enable();
     struct pcb *pcb = proc_init((struct rect){ 0, 0, 80, 24 });
     sched_init(pcb);
     intr_set_handler(INTR_TIMER, timer_handler);
     intr_set_handler(INTR_SYSCALL, syscall_handler);
     intr_set_handler(INTR_EXTERNAL, interrupt_handler);
     mtime_reset(QUANTUM);
-    ctx_user_setup();
 
-    sched_run(taskA, (struct rect){ 0,   0,  40, 12 });  // upper-left
-    sched_run(taskB, (struct rect){ 40,  0,  40, 12 });  // upper-right
-    sched_run(taskA, (struct rect){ 0,  12,  40, 12 });  // lower-left
+    sched_run(taskA, (struct rect){ 0,  0,  40, 12 });  // upper-left
+    sched_run(taskA, (struct rect){ 0, 12,  40, 12 });  // lower-left
 
     sched_idle();
 }
