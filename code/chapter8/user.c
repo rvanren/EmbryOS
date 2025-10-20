d2f73cae479bcece7ed93550078f96f76577f275
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-20 16:11:17 -0400

    Latest

--- code/chapter8/user.c
+++ code/chapter8/user.c
@@ -1,7 +1,10 @@
 #include "syslib.h"
 
 void main(void) {
-    user_put(0, 0, 'X', 0, 7);
-    for (;;)
-        ;
+    user_put(10, 3, '$', 2, 0);
+    for (int counter = 0;; counter++) {
+        char c = user_get();
+        user_put(10, 5 + counter % 10, c, 2, 0);
+        if (c == '.') user_exit();
+    }
 }
