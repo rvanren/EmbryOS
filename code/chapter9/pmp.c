f820e5123b4e03b250d95077ba78cdb5f8f97421
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-21 14:32:13 -0400

    Latest

--- code/chapter9/pmp.c
+++ code/chapter9/pmp.c
@@ -3,13 +3,11 @@
 #include "frame.h"
 #include "process.h"
 
-// CSR helpers (if you don’t already have them)
-#define read_csr(reg) ({ unsigned long __tmp; \
+#define read_csr(reg) ({ uintptr_t __tmp; \
   __asm__ volatile ("csrr %0, " #reg : "=r"(__tmp)); __tmp; })
 #define write_csr(reg, val) ({ \
   __asm__ volatile ("csrw " #reg ", %0" :: "rK"(val)); })
 
-// PMP config bits
 enum {
   PMP_R = 1 << 0,
   PMP_W = 1 << 1,
@@ -18,23 +16,10 @@ enum {
   PMP_L = 1 << 7,
 };
 
-// Encode NAPOT pmpaddr from base/size (size power-of-two >= 8)
-static inline unsigned long pmp_napot_addr(uintptr_t base, size_t size) {
+static inline uintptr_t pmp_napot_addr(uintptr_t base, size_t size) {
   return (base >> 2) | ((size >> 1) - 1);
 }
 
-#ifdef notdef
-// Set cfg byte i in pmpcfg0 (entries 0..3)
-static inline void pmpcfg0_set_byte(int i, unsigned char cfg) {
-  unsigned long c = read_csr(pmpcfg0);
-  unsigned shift = (i & 3) * 8;
-  c &= ~(0xFFUL << shift);
-  c |= ((unsigned long)cfg) << shift;
-  write_csr(pmpcfg0, c);
-}
-#endif
-
-// Compute the PMP configuration for a process
 void pmp_config(struct pcb *pcb) {
     pcb->pmp.addr[0] = pmp_napot_addr((uintptr_t) pcb->base, PAGE_SIZE);
     pcb->pmp.addr[1] = pmp_napot_addr((uintptr_t) pcb->stack, PAGE_SIZE);
@@ -44,12 +29,8 @@ void pmp_config(struct pcb *pcb) {
 
 // Set PMP registers (before each mret)
 void pmp_load(struct pcb *pcb) {
-    // write pmpaddr registers
     write_csr(pmpaddr0, pcb->pmp.addr[0]);
     write_csr(pmpaddr1, pcb->pmp.addr[1]);
-
-    // write pmpcfg0 (assume only using entries 0–1)
-    unsigned long cfg = ((unsigned long) pcb->pmp.cfg[0]) |
-                        ((unsigned long) pcb->pmp.cfg[1] << 8);
-    write_csr(pmpcfg0, cfg);
+    write_csr(pmpcfg0, cfg = ((uintptr_t) pcb->pmp.cfg[0]) |
+                        ((uintptr_t) pcb->pmp.cfg[1] << 8));
 }
