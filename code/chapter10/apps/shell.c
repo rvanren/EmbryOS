92231833f135191312a05b754921e9ab232ee6af
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-22 07:54:59 -0400

    Latest

--- code/chapter10/apps/shell.c
+++ code/chapter10/apps/shell.c
@@ -2,17 +2,19 @@
 #include "screen.h"
 #include "string.h"
 
+#define N_RECTS     4       // number of windows on the screen
+#define N_APPS      3       // number of available applications to run
+
 void printf(struct screen *screen, const char *s) {
     while (*s != 0) putchar(screen, *s++);
 }
 
-char *apps[4];
+char *apps[N_APPS];
 struct rect {
-    const char *name;
-    char x, y;   // top-left corner on global screen
-    char w, h;   // width and height
+    const char *name;   // name of window
+    char x, y, w, h;    // bounding box
 };
-struct rect rects[5];
+struct rect rects[N_RECTS];
 
 void exec(struct screen *screen, char *line) {
     char *argv[64];
@@ -36,65 +38,41 @@ void exec(struct screen *screen, char *line) {
     }
 
     int r = 0;
-    while (rects[r].name != 0) {
+    while (r < N_RECTS) {
         if (strcmp(rects[r].name, argv[0]) == 0) break;
         r++;
     }
-    if (rects[r].name == 0) {
-        printf(screen, "Unknown command\n");
-        return;
-    }
+    if (r == N_RECTS) { printf(screen, "Unknown command\n"); return; }
 
-    if (argc == 1) {
-        printf(screen, "Too few arguments\n");
-        return;
-    }
-
+    if (argc == 1) { printf(screen, "Too few arguments\n"); return; }
     int app = 0;
-    while (apps[app] != 0) {
+    while (app < N_APPS) {
         if (strcmp(apps[app], argv[1]) == 0) break;
         app++;
     }
-    if (apps[app] == 0) {
-        printf(screen, "Unknown app\n");
-        return;
-    }
+    if (app == N_APPS) { printf(screen, "Unknown app\n"); return; }
     user_spawn(app, rects[r].x,  rects[r].y, rects[r].w, rects[r].h);
 }
 
 void main(void) {
     struct screen screen;
     char line[128];
-    unsigned int n = 0;
 
-    // Because of PIC, I can't have pointers-to-objects in global/static data
-    // Hence this awkward initialization
+    // Because we are using position-independent code, we can't use pointers
+    // to objects in global/static data. Hence this awkward initialization.
     rects[0] = (struct rect){ "ul",  0,  0, 40, 12 };
     rects[1] = (struct rect){ "ur", 40,  0, 40, 12 };
     rects[2] = (struct rect){ "ll",  0, 12, 40, 12 };
     rects[3] = (struct rect){ "lr", 40, 12, 40, 12 };
-    rects[4] = (struct rect){ 0,     0,  0,  0,  0 };
     apps[0] = "shell";
     apps[1] = "pretty";
     apps[2] = "crash";
-    apps[3] = 0;
 
     screen_init(&screen);
     screen_sync(&screen);
     printf(&screen, "$ ");
     for (;;) {
-        char c = user_get();
-        if (c == '\r') {
-            putchar(&screen, '\n');
-            line[n] = 0;
-            exec(&screen, line);
-            n = 0;
-            printf(&screen, "$ ");
-        }
-        else {
-            putchar(&screen, c);
-            if (c == '\b' || c == '\177') { if (n > 0) n--; }
-	        else if (n < sizeof(line) - 1) line[n++] = c;
-        }
+        kb_readline(&screen, line, sizeof(line));
+        exec(&screen, line);
     }
 }
