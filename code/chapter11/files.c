363579d0e679fabc927a587ed147923a8c8c03e5
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-22 16:36:29 -0400

    Latest

--- code/chapter11/files.c
+++ code/chapter11/files.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include "apps_gen.h"
+#include "app_info.h"
 #include "bd_ramdisk.h"
 #include "bd_simple.h"
 #include "flat.h"
@@ -11,18 +11,18 @@ struct block ramdisk_blocks[BLOCK_SIZE];
 
 int applications[MAX_APPS];
 
-static void write_app(int file_id, uint32_t gp,
+static void write_app(struct flat *ffs, int file_id, uint32_t gp,
                       const char *start, const char *end) {
     uint32_t size = (uint32_t)(end - start);
-    flat_write(&flat_fs, file_id, 0, &gp, 4);
-    flat_write(&flat_fs, file_id, 4, start, size);
+    flat_write(ffs, file_id, 0, &gp, 4);
+    flat_write(ffs, file_id, 4, start, size);
 }
 
 void files_init(void) {
     struct bd ramdisk_iface;
     struct ramdisk_state ramdisk_state;
     struct bd simple_iface;
-    struct inode_layer_state simple_state;
+    struct simple_state simple_state;
     struct flat flat_fs;
 
     ramdisk_init(&ramdisk_iface, &ramdisk_state,
@@ -34,7 +34,7 @@ void files_init(void) {
     for (int i = 0; i < n_applications; i++) {
         int f = flat_create(&flat_fs);
         applications[i] = f;
-        write_app(f, app_table[i].gp,
+        write_app(&ffs, f, app_table[i].gp,
                   app_table[i].start, app_table[i].end);
     }
 }
