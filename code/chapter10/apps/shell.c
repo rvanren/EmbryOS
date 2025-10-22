aee70d6743bfbc056ebf997810be9fa2cf556547
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-21 21:22:40 -0400

    Trying to reduce code size

--- code/chapter10/apps/shell.c
+++ code/chapter10/apps/shell.c
@@ -5,64 +5,69 @@
 #define NCOLS 40
 
 struct cell { char c, fg, bg; };
-struct cell screen[NROWS][NCOLS];
-int cur_col = 0, cur_fg = 0, cur_bg = 7;
-
-void screen_init() {
-    for (int row = 0; row < NROWS; row++) {
-        for (int col = 0; col < NCOLS; col++) {
-            screen[row][col] = (struct cell) { ' ', 0, 7 };
-        }
-    }
-}
+struct screen {
+    struct cell screen[NROWS][NCOLS];
+    int cur_col, cur_fg, cur_bg;
+};
 
 void screen_put(int row, int col, struct cell cell) {
     user_put(row, col, cell.c, cell.fg, cell.bg);
 }
 
-void screen_sync() {
+void screen_init(struct screen *screen) {
+    screen->cur_col = 0;
+    screen->cur_fg = 0;
+    screen->cur_bg = 7;
     for (int row = 0; row < NROWS; row++) {
         for (int col = 0; col < NCOLS; col++) {
-            screen_put(row, col, screen[row][col]);
+            screen->cells[row][col] = (struct cell) { ' ', 0, 7 };
         }
     }
 }
 
-void scroll() {
+void screen_sync(struct screen *screen) {
+    for (int row = 0; row < NROWS; row++) {
+        for (int col = 0; col < NCOLS; col++) {
+            screen_put(row, col, screen->cells[row][col]);
+        }
+    }
+}
+
+void screen_scroll(struct screen *screen) {
     for (int row = 0; row < NROWS - 1; row++) {
         for (int col = 0; col < NCOLS; col++) {
-            screen[row][col] = screen[row + 1][col];
+            screen->cells[row][col] = screen[row + 1][col];
         }
     }
     for (int col = 0; col < NCOLS; col++) {
-        screen[NROWS - 1][col] = (struct cell) { ' ', 0, 7 };
+        screen->cells[NROWS - 1][col] = (struct cell) { ' ', 0, 7 };
     }
-    screen_sync();
+    screen_sync(screen);
 }
 
-void putchar(char c) {
+void putchar(struct screen *screen, char c) {
     if (c == '\b' || c == '\177') {
-        if (cur_col > 0) {
-            cur_col--;
-            user_put(NROWS - 1, cur_col, ' ', cur_fg, cur_bg);
-            screen[NROWS - 1][cur_col] = (struct cell) { ' ', cur_fg, cur_bg };
-            if (cur_col > 0)
-                screen_put(NROWS - 1, cur_col - 1, screen[NROWS - 1][cur_col - 1]);
+        if (screen->cur_col > 0) {
+            screen->cur_col--;
+            user_put(NROWS - 1, screen->cur_col, ' ', screen->cur_fg, screen->cur_bg);
+            screen->cells[NROWS - 1][screen->cur_col] = (struct cell) { ' ', screen->cur_fg, screen->cur_bg };
+            if (screen->cur_col > 0)
+                screen_put(NROWS - 1, screen->cur_col - 1, screen->cells[NROWS - 1][screen->cur_col - 1]);
         }
         return;
     }
-    if (c == '\n' || cur_col == NCOLS) {
-        scroll();
-        cur_col = 0;
+    if (c == '\n' || screen->cur_col == NCOLS) {
+        screen_scroll(screen);
+        screen->cur_col = 0;
     }
     if (c != '\n') {
-        user_put(NROWS - 1, cur_col, c, cur_fg, cur_bg);
-        screen[NROWS - 1][cur_col] = (struct cell) { c, cur_fg, cur_bg };
-        cur_col++;
+        user_put(NROWS - 1, screen->cur_col, c, screen->cur_fg, screen->cur_bg);
+        screen->cells[NROWS - 1][screen->cur_col] = (struct cell) { c, screen->cur_fg, screen->cur_bg };
+        screen->cur_col++;
     }
 }
 
-static void print_unsigned(unsigned int x, unsigned int base) {
+static void print_unsigned(struct screen *screen, unsigned int x, unsigned int base) {
     char buf[16];
     int i = 0;
     do {
@@ -70,42 +75,42 @@ static void print_unsigned(unsigned int x, unsigned int base) {
         buf[i++] = (digit < 10) ? '0' + digit : 'a' + (digit - 10);
         x /= base;
     } while (x != 0);
-    while (--i >= 0) putchar(buf[i]);
+    while (--i >= 0) putchar(screen, buf[i]);
 }
 
-void printf(const char *fmt, ...) {
+void printf(struct screen *screen, const char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     for (; *fmt; fmt++) {
-        if (*fmt != '%') { putchar(*fmt); continue; }
+        if (*fmt != '%') { putchar(screen, *fmt); continue; }
         fmt++;
         switch (*fmt) {
         case 'd': { int x = va_arg(ap, int);
-            if (x < 0) { putchar('-'); x = -x; }
-            print_unsigned((unsigned int) x, 10);
+            if (x < 0) { putchar(screen, '-'); x = -x; }
+            print_unsigned(screen, (unsigned int) x, 10);
             break;
         }
         case 'u': { unsigned int x = va_arg(ap, unsigned int);
-            print_unsigned(x, 10);
+            print_unsigned(screen, x, 10);
             break;
         }
         case 'x': { unsigned int x = va_arg(ap, unsigned int);
-            print_unsigned(x, 16);
+            print_unsigned(screen, x, 16);
             break;
         }
         case 's': { char *s = va_arg(ap, char *);
-            while (*s) putchar(*s++);
+            while (*s) putchar(screen, *s++);
             break;
         }
         case 'c': { int c = va_arg(ap, int);
-            putchar(c);
+            putchar(screen, c);
             break;
         }
         case '%':
-            putchar('%');
+            putchar(screen, '%');
             break;
         default:
-            putchar('%'); putchar(*fmt);
+            putchar(screen, '%'); putchar(screen, *fmt);
         }
     }
     va_end(ap);
@@ -131,7 +136,7 @@ struct rect rects[] = {
     { "ll",  0, 12, 40, 12 },
     { "lr", 40, 12, 40, 12 },
     { 0,     0,  0,  0,  0 }
-}
+};
 
 void exec(char *line) {
     char *argv[64];
@@ -149,7 +154,7 @@ void exec(char *line) {
 
     if (strcmp(argv[0], "quit") == 0) user_exit();
     if (strcmp(argv[0], "exit") == 0) user_exit();
-    if (strcmp(argv[0], "help") {
+    if (strcmp(argv[0], "help") == 0) {
         printf("[ul|ur|ll|lr] [shell|pretty|crash]\n");
         return;
     }
@@ -182,24 +187,25 @@ void exec(char *line) {
 }
 
 void main(void) {
+    struct screen screen;
     char line[128];
     unsigned int n;
 
-    screen_init();
-    screen_sync();
-    putchar('$'); putchar(' ');
+    screen_init(&screen);
+    screen_sync(&screen);
+    putchar(&screen, '$'); putchar(&screen, ' ');
     for (;;) {
         char c = user_get();
         if (c == '\r') {
-            putchar('\n');
+            putchar(&screen, '\n');
             line[n] = 0;
             exec(line);
             n = 0;
-            putchar('$');
-            putchar(' ');
+            putchar(&screen, '$');
+            putchar(&screen, ' ');
         }
         else {
-            putchar(c);
+            putchar(&screen, c);
             if (n < sizeof(line) - 1) line[n++] = c;
         }
     }
