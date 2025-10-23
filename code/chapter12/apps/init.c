64e4f5a5d6c41a16414a732e43649be83dbae1c0
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-23 04:26:28 -0400

    Latest

--- code/chapter12/apps/init.c
+++ code/chapter12/apps/init.c
@@ -1,20 +1,11 @@
 #include "syslib.h"
-#include "string.h"
-#include "screen.h"
+#include "stdio.h"
 #include "dir.h"
 
-void printf(struct screen *screen, const char *s) {
-    while (*s != 0) screen_putchar(screen, *s++);
-}
-
 void main(void) {
-    struct screen screen;
     char line[128];
 
-    screen_init(&screen);
-    screen_sync(&screen);
-
-    printf(&screen, "Initializing directory...\n");
+    printf("Initializing directory...\n");
 
     dir_create("init", 2);
     dir_create("shell", 3);
@@ -22,7 +13,7 @@ void main(void) {
     dir_create("crash", 5);
     dir_create("ls", 6);
 
-    printf(&screen, "System ready.\n");
+    printf("System ready.\n");
 
     user_spawn(dir_lookup("shell"), 0, 0, 40, 12);
     user_exit();
