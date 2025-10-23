190832852e17eea8b5f94bf079e3b56bdd8728a6
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-23 11:32:47 -0400

    Latest

--- code/chapter13/syscall.c
+++ code/chapter13/syscall.c
@@ -6,16 +6,17 @@
 #include "sched.h"
 #include "uart.h"
 #include "flat.h"
+#include "frame.h"
 
-void check_legal(struct pcb *p, uintptr_t start, int size) {
+void check_legal(struct pcb *self, uintptr_t start, int size) {
     uintptr_t end = start + size - 1;
     if (end < start) {
         proc_put(self, 0, 0, '>', 0, 1);
         printf("bad system call size<");
         proc_exit();
     }
-    uintptr_t base_lo  = (uintptr_t) p->base, base_hi  = base_lo + PAGE_SIZE - 1;
-    uintptr_t stack_lo = (uintptr_t) p->stack, stack_hi = stack_lo + PAGE_SIZE - 1;
+    uintptr_t base_lo  = (uintptr_t) self->base, base_hi  = base_lo + PAGE_SIZE - 1;
+    uintptr_t stack_lo = (uintptr_t) self->stack, stack_hi = stack_lo + PAGE_SIZE - 1;
     if (!((start >= base_lo  && end <= base_hi) || (start >= stack_lo && end <= stack_hi))) {
         proc_put(self, 0, 0, '>', 0, 1);
         printf("bad system call address<");
