27f465220f387df5c0d69266d9418643a93f5a19
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-22 21:19:03 -0400

    Fixed

--- code/chapter12/syslib.h
+++ code/chapter12/syslib.h
@@ -43,27 +43,27 @@ static inline int user_create(void) {
 
 static inline int user_read(int file, int off, void *dst, int n) {
     register int a7 asm("a7") = SYS_READ;
-    yegister int a0 asm("a0") = file;
+    register int a0 asm("a0") = file;
     register int a1 asm("a1") = off;
     register void *a2 asm("a2") = dst;
     register int a3 asm("a3") = n;
-    asm volatile("ecall" : "=r"(a0) : "r"(a0), "r"(a1), "r"(a2), "r"(a3)", "r"(a7));
+    asm volatile("ecall" : "=r"(a0) : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a7));
     return a0;
 }
 
 static inline int user_write(int file, int off, const void *src, int n) {
     register int a7 asm("a7") = SYS_WRITE;
-    yegister int a0 asm("a0") = file;
+    register int a0 asm("a0") = file;
     register int a1 asm("a1") = off;
-    register void *a2 asm("a2") = src;
+    register const void *a2 asm("a2") = src;
     register int a3 asm("a3") = n;
-    asm volatile("ecall" : "=r"(a0) : "r"(a0), "r"(a1), "r"(a2), "r"(a3)", "r"(a7));
+    asm volatile("ecall" : "=r"(a0) : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a7));
     return a0;
 }
 
 static inline int user_size(int file) {
     register int a7 asm("a7") = SYS_SIZE;
-    yegister int a0 asm("a0") = file;
+    register int a0 asm("a0") = file;
     asm volatile("ecall" : "=r"(a0) : "r"(a0), "r"(a7));
     return a0;
 }
