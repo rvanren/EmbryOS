3f238d1d0c3ae537a6c6451b6703837417c8d529
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-22 21:25:47 -0400

    Latest

--- code/chapter12/apps/shell_aux.c
+++ code/chapter12/apps/shell_aux.c
@@ -32,10 +32,6 @@ void exec(struct screen *screen, char *line) {
 
     if (strcmp(argv[0], "exit") == 0) user_exit();
     if (strcmp(argv[0], "quit") == 0) user_exit();
-    if (strcmp(argv[0], "help") == 0) {
-        printf(screen, "[ul|ur|ll|lr] [shell|pretty|crash]\n");
-        return;
-    }
 
     int r = 0;
     while (r < N_RECTS && strcmp(rects[r].name, argv[0]) != 0) r++;
@@ -44,7 +40,10 @@ void exec(struct screen *screen, char *line) {
     if (argc == 1) { printf(screen, "Too few arguments\n"); return; }
     int a = 0;
     while (a < N_APPS && strcmp(apps[a].name, argv[1]) != 0) a++;
-    if (a == N_APPS) { printf(screen, "Unknown app\n"); return; }
+    if (a == N_APPS) {
+        printf(screen, "Usage: [ul|ur|ll|lr] [shell|pretty|crash]\n");
+        return;
+    }
 
     user_spawn(apps[a].file, rects[r].x, rects[r].y, rects[r].w, rects[r].h);
 }
