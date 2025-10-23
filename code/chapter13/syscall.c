dd6eb3e57dbc41a66b3642422680d0e3b2829c63
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-23 11:29:52 -0400

    Latest

--- code/chapter13/syscall.c
+++ code/chapter13/syscall.c
@@ -7,6 +7,18 @@
 #include "uart.h"
 #include "flat.h"
 
+void check_legal(struct pcb *p, uintptr_t start, size_t size) {
+    uintptr_t end = start + size - 1;
+    if (end < start) return 0;
+    uintptr_t base_lo  = (uintptr_t) p->base, base_hi  = base_lo + PAGE_SIZE - 1;
+    uintptr_t stack_lo = (uintptr_t) p->stack, stack_hi = stack_lo + PAGE_SIZE - 1;
+    if (!((start >= base_lo  && end <= base_hi) || (start >= stack_lo && end <= stack_hi))) {
+        proc_put(self, 0, 0, '>', 0, 1);
+        printf("bad system call address<");
+        proc_exit();
+    }
+}
+
 void syscall_handler(struct trap_frame *tf) {
     struct pcb *self = run_queue[proc_current]->next;
     extern struct flat flat_fs;
@@ -19,7 +31,7 @@ void syscall_handler(struct trap_frame *tf) {
         proc_exit();
         break;
     case SYS_SPAWN:
-        // TODO must check legality of buffer
+        check_legal(self, (uintptr_t) tf->a5, tf->a6);
         sched_run(tf->a0, (struct rect){ tf->a1, tf->a2, tf->a3, tf->a4 }, (void *) (uintptr_t) tf->a5, tf->a6, exec_user);
         break;
     case SYS_PUT:
@@ -33,11 +45,11 @@ void syscall_handler(struct trap_frame *tf) {
         tf->a0 = flat_create(&flat_fs);
         break;
     case SYS_READ:
-        // TODO must check legality of buffer
+        check_legal(self, (uintptr_t) tf->a2, tf->a3);
         tf->a0 = flat_read(&flat_fs, tf->a0, tf->a1, (void *) (uintptr_t) tf->a2, tf->a3);
         break;
     case SYS_WRITE:
-        // TODO must check legality of buffer
+        check_legal(self, (uintptr_t) tf->a2, tf->a3);
         tf->a0 = flat_write(&flat_fs, tf->a0, tf->a1, (void *) (uintptr_t) tf->a2, tf->a3);
         break;
     case SYS_SIZE:
