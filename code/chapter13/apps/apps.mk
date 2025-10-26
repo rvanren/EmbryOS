ccc696ed6b471e177efa08f4e818d643dfce78a2
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-26 13:57:05 -0400

    Latest

--- code/chapter13/apps/apps.mk
+++ code/chapter13/apps/apps.mk
@@ -1,3 +1,3 @@
 # The list of applications to be compiled into the kernel image is here
 # Note that the kernel must know the order of these applications.
-APPS = init shell life crash ls cat snake splash
+APPS = cat crash help init life ls shell snake splash
