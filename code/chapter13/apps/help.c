4b9e88a62f66b79737c1d2c3b5023e8319841ba8
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-26 13:57:35 -0400

    Latest

---
+++ code/chapter13/apps/help.c
@@ -0,0 +1,17 @@
+#include "syslib.h"
+#include "stdio.h"
+#include "dir.h"
+
+char help[] =
+//   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    "* There are 4 windows: ul ur ll lr.\n"
+    "* The line syntax is [ul|ur|ll|lr] cmd\n"
+    "* This runs cmd in the given window.\n"
+    "* The default window is ur.\n"
+    "* Change default by just [ul|ur|ll|lr]\n"
+    "* Exit by typing exit or quit.\n"
+    "* Change window focus using TAB\n"
+
+void main(void) {
+    printf("%s", help);
+}
