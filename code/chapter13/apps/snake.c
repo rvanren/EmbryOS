bfae7fa1ba1046562729fc9c63788ef98918e916
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-26 12:38:22 -0400

    Latest

--- code/chapter13/apps/snake.c
+++ code/chapter13/apps/snake.c
@@ -12,6 +12,10 @@ static int length = 3;
 static int dir_r = 0, dir_c = 1;
 static int moves = 0;
 
+// ----------------------------------------------------------------------
+// Drawing helpers
+// ----------------------------------------------------------------------
+
 static void draw_cell(int r, int c, char ch) {
     user_put(r, c, CELL(ch, ANSI_GREEN, ANSI_BLACK));
 }
@@ -29,6 +33,10 @@ static void init_snake(void) {
         draw_cell(snake[i].r, snake[i].c, 'o');
 }
 
+// ----------------------------------------------------------------------
+// Collision
+// ----------------------------------------------------------------------
+
 static int hits_body(int r, int c) {
     for (int i = 0; i < length; i++)
         if (snake[i].r == r && snake[i].c == c)
@@ -36,12 +44,15 @@ static int hits_body(int r, int c) {
     return 0;
 }
 
-static void move_snake(void) {
-    point_t head = snake[length - 1];
-    int new_r = head.r + dir_r;
-    int new_c = head.c + dir_c;
+// ----------------------------------------------------------------------
+// Movement logic
+// ----------------------------------------------------------------------
 
-    // stop at borders or collisions
+static void move_snake(void) {
+    point_t old_head = snake[length - 1];
+    int new_r = old_head.r + dir_r;
+    int new_c = old_head.c + dir_c;
+
     if (new_r < 0 || new_r >= HEIGHT || new_c < 0 || new_c >= WIDTH)
         return;
     if (hits_body(new_r, new_c))
@@ -49,32 +60,29 @@ static void move_snake(void) {
 
     int grow = (moves % GROW_INTERVAL == GROW_INTERVAL - 1 && length < MAXLEN);
 
-    if (grow) {
-        // grow: shift everything one step to the RIGHT
-        for (int i = length; i > 0; i--)
-            snake[i - 1] = snake[i - 1]; // dummy line for clarity
-        for (int i = length; i > 0; i--)
-            snake[i - 1] = snake[i - 1]; // (we’ll actually shift below)
-        for (int i = length; i > 0; i--)
-            snake[i - 1] = snake[i - 1]; // (ignore redundant clarity)
-        // the real shift:
+    if (!grow) {
+        clear_cell(snake[0].r, snake[0].c);   // erase tail
+        for (int i = 0; i < length - 1; i++)
+            snake[i] = snake[i + 1];
+    } else {
         for (int i = length; i > 0; i--)
             snake[i] = snake[i - 1];
         length++;
-    } else {
-        // normal move: erase tail, shift LEFT
-        clear_cell(snake[0].r, snake[0].c);
-        for (int i = 0; i < length - 1; i++)
-            snake[i] = snake[i + 1];
     }
 
-    // new head
     snake[length - 1] = (point_t){new_r, new_c};
 
-    // redraw body
+    // Redraw previous head as body
+    draw_cell(old_head.r, old_head.c, 'o');
+
+    // Redraw all other body segments (just to be safe)
     for (int i = 0; i < length - 1; i++)
         draw_cell(snake[i].r, snake[i].c, 'o');
 }
+
+// ----------------------------------------------------------------------
+// Main
+// ----------------------------------------------------------------------
 
 int main(void) {
     init_snake();
@@ -83,8 +91,8 @@ int main(void) {
         int key = user_get(
             snake[length - 1].r,
             snake[length - 1].c,
-            CELL('@', ANSI_YELLOW, ANSI_BLACK),
-            CELL('@', ANSI_BLUE, ANSI_BLACK)
+            CELL('@', ANSI_YELLOW, ANSI_BLACK), // focused
+            CELL('@', ANSI_BLUE,   ANSI_BLACK)  // unfocused
         );
 
         int new_dr = dir_r, new_dc = dir_c;
