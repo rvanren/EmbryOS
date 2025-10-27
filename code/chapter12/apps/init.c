ebef35f2c16cdb7de22b80065997f47f1c445c89
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 18:38:37 -0400

    Latest

--- code/chapter12/apps/init.c
+++ code/chapter12/apps/init.c
@@ -2,19 +2,6 @@
 #include "stdio.h"
 #include "dir.h"
 
-#ifdef CH13
-char README[] =
-//   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    "This is EmbryOS\n"
-    "Type help in the shell for help\n"
-    "These are the current apps:\n"
-    "* cat [files] - output files\n"
-    "* life        - Game of Life\n"
-    "* ls          - directory listing\n"
-    "* shell       - command shell\n"
-    "* snake       - snake game\n";
-#endif
-
 void init_main(void) {
     printf("Initializing directory...\n");
 
@@ -26,16 +13,6 @@ void init_main(void) {
     dir_create("shell",  6);
     dir_create("crash",  7);
     dir_create("ls",     8);
-
-#ifdef CH13
-    dir_create("cat",    9);
-    dir_create("help",  10);
-
-    int readme = user_create();
-    user_write(readme, 0, README, sizeof(README) - 1);
-    dir_create("README", readme);
-#endif
-
     user_spawn(dir_lookup("shell"),   0,  0, 40, 12, 0, 0);
     user_spawn(dir_lookup("splash"), 40,  0, 40, 12, 0, 0);
 }
