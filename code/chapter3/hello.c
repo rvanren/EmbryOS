d2bbd4f86dbc1d104ddf1ba0dc6ce9e598f7f47d
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 13:19:27 -0400

    Latest

--- code/chapter3/hello.c
+++ code/chapter3/hello.c
@@ -1,38 +1,21 @@
-#include "frame.h"
 #include "sched.h"
-#include "stdio.h"
-#include "ctx.h"
+#include "kprintf.h"
+#include "syscall.h"
 #include "uart.h"
+#include "screen.h"
+#include "interrupt.h"
 
-void taskA(void) {
-    struct pcb *self = run_queue->next;
-    int col = 0;
-    while (1) {
-        proc_put(self, 1, col, 'A', 2, 0);
-        col = (col + 1) % self->area.w;
-        for (volatile int i = 0; i < 100000; i++) sched_yield();
-    }
-}
-
-void taskB(void) {
-    struct pcb *self = run_queue->next;
-    int row = 0;
-    while (1) {
-        proc_put(self, row, 5, 'B', 4, 0);
-        row = (row + 1) % self->area.h;
-        for (volatile int i = 0; i < 100000; i++) sched_yield();
-    }
+void exception_handler(struct trap_frame *tf) {
+    kprintf("trap: cause=%d mepc=%x mtval=%x<",
+                        tf->mcause & 0xFFF, tf->mepc, tf->mtval);
 }
 
 int main(void) {
-    frame_init(); uart_init();
-    struct pcb *pcb = proc_init((struct rect){ 0, 0, 80, 24 });
-    sched_init(pcb);
-
-    sched_run(taskA, (struct rect){  0,  0, 40, 12 });  // upper-left
-    sched_run(taskB, (struct rect){ 40,  0, 40, 12 });  // upper-right
-    sched_run(taskB, (struct rect){  0, 12, 40, 12 });  // lower-left
-    sched_run(taskA, (struct rect){ 40, 12, 40, 12 });  // lower-left
-
-    sched_idle();
+    uart_init();
+    intr_init();
+    intr_set_handler(INTR_EXCEPTION, exception_handler);
+    screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
+                        CELL(' ', ANSI_WHITE, ANSI_BLACK));
+    extern void splash_main();
+    splash_main();
 }
