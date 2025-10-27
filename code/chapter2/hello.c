1fd772ed330933b5e1c46fac54475c881dca08b5
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 13:02:48 -0400

    Latest

--- code/chapter2/hello.c
+++ code/chapter2/hello.c
@@ -1,35 +1,102 @@
-#include "stdio.h"
-#include "screen.h"
+#include "sched.h"
+#include "kprintf.h"
+#include "syscall.h"
 #include "uart.h"
+#include "screen.h"
 
-void taskA(void) {
-    const char *banner = "==== EmbryOS says Hello World ====   ";
-    const int len   = 38;                 // length of banner string (include spaces)
-    const int row   = 12;                 // vertical position (middle of 24-line screen)
-    const int width = SCREEN_COLS;        // logical screen width
-    int color = 1;
-    int offset = 0;
+#ifdef CH3
+#include "interrupt.h"
 
-    screen_clear(0, 0, SCREEN_COLS, SCREEN_ROWS, 4);
+#ifdef CH4
+#include "frame.h"
+#include "ctx.h"
+#endif
 
-    while (1) {
-        // draw one frame of the scrolling banner
-        for (int i = 0; i < width; i++) {
-            char c = banner[(i + offset) % len];
-            screen_move(row, i);
-            screen_put(c, color, 0);
-        }
-
-        // cycle color & offset for next frame
-        color = (color % 7) + 1;
-        offset = (offset + 1) % len;
-
-        // delay
-        for (volatile int i = 0; i < 10000000; i++) ;
-    }
+void exception_handler(struct trap_frame *tf) {
+#ifdef CH4
+    struct pcb *self = run_queue[proc_current]->next;
+    proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
+#endif
+    kprintf("trap: cause=%d mepc=%x mtval=%x<",
+                        tf->mcause & 0xFFF, tf->mepc, tf->mtval);
+#ifdef CH4
+    proc_exit();
+#endif
 }
+#endif
+
+#ifdef CH5
+#include "mtime.h"
+
+#define QUANTUM          50000        // 50 milliseconds
+
+void timer_handler(struct trap_frame *tf) {
+    sched_yield();
+    mtime_reset(QUANTUM); // add another quantum
+}
+#endif
+
+#ifdef CH6
+#include "plic.h"
+#endif
+
+#ifdef CH7
+#include "pmp.h"
+#endif
+
+#ifdef CH11
+#include "files.h"
+#endif
 
 int main(void) {
     uart_init();
-    taskA();
+
+#ifdef CH3
+    intr_init();
+    intr_set_handler(INTR_EXCEPTION, exception_handler);
+#endif
+
+#ifdef CH6
+    plic_init();
+#endif
+
+#ifdef CH7
+    pmp_init();
+#endif
+
+#ifdef CH11
+    files_init();
+#endif
+
+#ifdef CH4
+    frame_init();
+    struct pcb *pcb = proc_init((struct rect){ 0, 0, 80, 24 });
+    sched_init(pcb);
+#endif
+
+#ifdef CH7
+    extern void syscall_handler(struct trap_frame *);
+    intr_set_handler(INTR_SYSCALL, syscall_handler);
+#endif
+
+#ifdef CH6
+    intr_set_handler(INTR_EXTERNAL, plic_handler);
+#endif
+
+#ifdef CH5
+    mtime_init();
+    intr_set_handler(INTR_TIMER, timer_handler);
+    mtime_reset(QUANTUM);
+#endif
+
+    screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
+                        CELL(' ', ANSI_WHITE, ANSI_BLACK));
+
+#ifdef CH4
+    sched_run(2, (struct rect){ 0, 0, 39, 11 }, 0, 0);  // run init process
+    sched_idle();
+#else
+    extern void splash_main();
+    splash_main();
+#endif
 }
