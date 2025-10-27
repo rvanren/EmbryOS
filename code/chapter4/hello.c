494dbc643576bd549690f39b2a0d775e2edb83f0
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 13:53:12 -0400

    Latest

--- code/chapter4/hello.c
+++ code/chapter4/hello.c
@@ -1,9 +1,8 @@
-#include "frame.h"
 #include "sched.h"
 #include "kprintf.h"
-#include "ctx.h"
 #include "syscall.h"
 #include "uart.h"
+#include "screen.h"
 #include "interrupt.h"
 
 #ifdef CH6
@@ -70,6 +69,9 @@ int main(void) {
     intr_set_handler(INTR_TIMER, timer_handler);
     mtime_reset(QUANTUM);
 #endif
+
+    screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
+                        CELL(' ', ANSI_WHITE, ANSI_BLACK));
 
     sched_run(2, (struct rect){ 0, 0, 39, 11 }, 0, 0);  // run init process
     sched_idle();
