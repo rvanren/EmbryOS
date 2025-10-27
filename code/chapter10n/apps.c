c8090262990c16c7cf5d9906f4b4ec3777debbaf
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-26 22:48:32 -0400

    Starting Chapter 10n (remove file system)

---
+++ code/chapter10n/apps.c
@@ -0,0 +1,54 @@
+#include <stddef.h>
+#include <stdint.h>
+#include "frame.h"
+#include "sched.h"
+#include "pmp.h"
+#include "stdio.h"
+#include "string.h"
+#include "flat.h"
+
+__attribute__((noreturn))
+void enter_user(void *entry, uintptr_t gp_val,
+                uintptr_t user_sp, size_t arg_size, uintptr_t ksp);
+
+void exec_user(void) {
+    extern struct flat flat_fs;
+    struct pcb *self = run_queue[proc_current]->next;
+
+    uint32_t gp_offset;
+    flat_read(&flat_fs, self->executable, 0, &gp_offset, sizeof(gp_offset));
+
+    uint32_t size = flat_size(&flat_fs, self->executable) - sizeof(gp_offset);
+    if (size > PAGE_SIZE) {
+        proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
+        printf("executable too large<");
+        proc_exit();
+    }
+
+    self->base = frame_alloc();
+    self->stack = frame_alloc();
+    if (self->base == 0 || self->stack == 0) {
+        proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
+        printf("out of memory<");
+        proc_exit();
+    }
+
+    // Initialize code/data page
+    flat_read(&flat_fs, self->executable, sizeof(gp_offset), self->base, size);
+    memset(&self->base[size], 0, PAGE_SIZE - size);
+    memset(self->stack, 0, PAGE_SIZE);
+
+    uintptr_t sp = (uintptr_t) self->stack + PAGE_SIZE;
+#ifdef CH13
+    // Initialize stack page
+    sp -= self->size;
+    sp &= ~0xF;   // align down to 16 bytes
+    memcpy((void *) sp, self->args, self->size);
+#endif
+
+    // Load PMP registers
+    pmp_load(self);
+
+    enter_user(self->base, (uintptr_t) (self->base + gp_offset), sp, self->size,
+                            (uintptr_t) self + PAGE_SIZE);
+}
---
+++ code/chapter10n/screen.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "screen.h"
+#include "uart.h"
+
+void screen_put(int row, int col, cell_t cell) {
+    if (row < 0) row = 0;
+    if (row >= SCREEN_ROWS) row = SCREEN_ROWS - 1;
+    if (col < 0) col = 0;
+    if (col >= SCREEN_COLS) col = SCREEN_COLS - 1;
+    printf("\033[%d;%dH", row + 1, col + 1);
+    printf("\033[3%dm\033[4%dm", CELL_FG(cell) % 8, CELL_BG(cell) % 8);
+    char ch = CELL_CH(cell);
+    if (ch < 32 || ch > 126) return;  // ignore non-printable
+    putchar(CELL_CH(ch));
+}
+
+void screen_fill(int x, int y, int w, int h, cell_t cell) {
+    printf("\033[?25l");    // hide cursor (we simulate our own)
+
+    if (x < 0) { w += x; x = 0; }
+    if (y < 0) { h += y; y = 0; }
+    if (x + w > SCREEN_COLS) w = SCREEN_COLS - x;
+    if (y + h > SCREEN_ROWS) h = SCREEN_ROWS - y;
+    if (w <= 0 || h <= 0) return;
+
+    for (int r = y; r < y + h; r++)
+        for (int c = 0; c < w; c++) screen_put(r, c, cell);
+}
