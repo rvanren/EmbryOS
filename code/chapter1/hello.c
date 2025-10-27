a4c4062d3d6c1d3f2e8c0eda006a12ada095ebe5
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 12:49:38 -0400

    Latest

--- code/chapter1/hello.c
+++ code/chapter1/hello.c
@@ -1,23 +1,111 @@
-#include <stdint.h>
+#include "frame.h"
+#include "sched.h"
+#include "kprintf.h"
+#include "ctx.h"
+#include "syscall.h"
+#include "uart.h"
 
-struct uart { uint32_t txdata; };
+#ifdef CH2
+#include "screen.h"
+#endif
 
-// Memory mapped IO:
-#define UART ((volatile struct uart *) 0x10010000)
-#define FULL (1 << 31)
+#ifdef CH3
+#include "interrupt.h"
 
-void putchar(char c) {
-    while (UART->txdata & FULL)
-        ;
-    UART->txdata = c;
+void exception_handler(struct trap_frame *tf) {
+#ifdef CH4
+    struct pcb *self = run_queue[proc_current]->next;
+    proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
+#endif
+    kprintf("trap: cause=%d mepc=%x mtval=%x<",
+                        tf->mcause & 0xFFF, tf->mepc, tf->mtval);
+#ifdef CH4
+    proc_exit();
+#endif
 }
+#endif
 
-void printf(char *str) {
-    while (*str != 0)
-        putchar(*str++);
+#ifdef CH5
+#include "mtime.h"
+
+#define QUANTUM          50000        // 50 milliseconds
+
+void timer_handler(struct trap_frame *tf) {
+    sched_yield();
+    mtime_reset(QUANTUM); // add another quantum
 }
+#endif
 
-int main() {
-    printf("Hello World\n");
-    return 0;
+#ifdef CH6
+#include "plic.h"
+#endif
+
+#ifdef CH7
+#include "pmp.h"
+#endif
+
+#ifdef CH11
+#include "files.h"
+#endif
+
+int main(void) {
+    uart_init();
+
+#ifdef CH3
+    intr_init();
+    intr_set_handler(INTR_EXCEPTION, exception_handler);
+#endif
+
+#ifdef CH6
+    plic_init();
+#endif
+
+#ifdef CH7
+    pmp_init();
+#endif
+
+#ifdef CH11
+    files_init();
+#endif
+
+#ifdef CH4
+    frame_init();
+    struct pcb *pcb = proc_init((struct rect){ 0, 0, 80, 24 });
+    sched_init(pcb);
+#endif
+
+#ifdef CH7
+    extern void syscall_handler(struct trap_frame *);
+    intr_set_handler(INTR_SYSCALL, syscall_handler);
+#endif
+
+#ifdef CH6
+    intr_set_handler(INTR_EXTERNAL, plic_handler);
+#endif
+
+#ifdef CH5
+    mtime_init();
+    intr_set_handler(INTR_TIMER, timer_handler);
+    mtime_reset(QUANTUM);
+#endif
+
+#ifdef CH2
+    screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
+                        CELL(' ', ANSI_WHITE, ANSI_BLACK));
+#endif
+
+#ifdef CH4
+    sched_run(2, (struct rect){ 0, 0, 39, 11 }, 0, 0);  // run init process
+    sched_idle();
+#else
+#ifdef CH2
+    extern void splash_main();
+    splash_main();
+#else
+    for (;;) {
+        kprintf("EmbryOS: Life Has Begun\n");
+        user_delay(1000);
+    }
+#endif
+#endif
 }
