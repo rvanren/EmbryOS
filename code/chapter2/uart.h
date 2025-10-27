cd5e789260b7554071f62e92455a0377bd7bd1d2
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 17:32:03 -0400

    Latest

--- code/chapter2/uart.h
+++ code/chapter2/uart.h
@@ -3,18 +3,5 @@
 // A UART (Universal Asynchronous Receiver Transmitter) is used for input/output to
 // simple serial peripherals (external devices) like a keyboard, mouse, or the screen.
 
-#ifdef CH4
-#include "process.h"
-#include "syscall.h"
-
-void uart_put(struct pcb *pcb, cell_t c);
-void uart_isr();        // the UART interrupt service routine
-void uart_exit(struct pcb *pcb);  // called when process exits
-#endif
-
 void uart_init(void);   // initialize this module
 void uart_putchar(char c);   // write a character to the screen
-
-#ifdef CH6
-int uart_get(struct pcb *self, int row, int col, cell_t cf, cell_t cu);
-#endif
