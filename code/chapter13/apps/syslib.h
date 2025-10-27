4950211197aad45811af28c0e334eec75d7daa47
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 19:00:44 -0400

    Latest

--- code/chapter13/apps/syslib.h
+++ code/chapter13/apps/syslib.h
@@ -13,18 +13,6 @@ static inline void user_spawn(int app, int row, char col, int wd, int ht,
     asm volatile("ecall" : : "r"(syscall_id) : "memory");
 }
 
-static inline void user_spawn(int app, int row, char col, int wd, int ht, const void *args, int sz) {
-    register int a7 asm("a7") = SYS_SPAWN;
-    register int a0 asm("a0") = app;
-    register int a1 asm("a1") = row;
-    register int a2 asm("a2") = col;
-    register int a3 asm("a3") = wd;
-    register int a4 asm("a4") = ht;
-    register const void *a5 asm("a5") = args;
-    register int a6 asm("a6") = sz;
-    asm volatile("ecall" : : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7));
-}
-
 static inline void user_put(int row, int col, cell_t cell) {
     register int a7 asm("a7") = SYS_PUT;
     register int a0 asm("a0") = row;
