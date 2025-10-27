76cb06f1d31338254c10c9e248531ae510c45144
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 15:22:13 -0400

    Latest

--- code/chapter11n/uart.h
+++ code/chapter11n/uart.h
@@ -8,6 +8,7 @@
 
 void putchar(char c);   // write a character to the screen and update the cursor
 void uart_init(void);   // initialize this module
+void uart_put(struct pcb *pcb, cell_t c);
 int uart_get(struct pcb *self, int row, int col, cell_t cf, cell_t cu);
 void uart_isr();        // the UART interrupt service routine
 void uart_exit(struct pcb *pcb);  // called when process exits
