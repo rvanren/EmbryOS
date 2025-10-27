b8bde6f230c6b199a74b9475e8bb1a0e52f168fe
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 11:14:24 -0400

    Latest

---
+++ code/chapter3n/process.c
@@ -0,0 +1,44 @@
+#include <stdint.h>
+#include "string.h"
+#include "frame.h"
+#include "process.h"
+#include "screen.h"
+
+struct pcb *proc_init(struct rect area){
+    screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
+                        CELL(' ', ANSI_WHITE, ANSI_BLACK));
+    struct pcb *pcb = frame_alloc();
+    memset(pcb, 0, sizeof(*pcb));
+    pcb->executable = -1;
+    pcb->area = area;
+    return pcb;
+}
+
+struct pcb *proc_create(int executable, struct rect area, void *args, int size) {
+    struct pcb *pcb = frame_alloc();
+    memset(pcb, 0, sizeof(*pcb));
+    pcb->executable = executable;
+    pcb->area = area;
+    pcb->args = args;
+    pcb->size = size;
+    return pcb;
+}
+
+void proc_put(struct pcb *pcb, int row, int col, cell_t cell) {
+    if (row < 0 || row >= pcb->area.h) return;
+    if (col < 0 || col >= pcb->area.w) return;
+    screen_put(pcb->area.y + row, pcb->area.x + col, cell);
+}
+
+void proc_enqueue(struct pcb **q, struct pcb *pcb) {
+    if (*q == 0) { *q = pcb; pcb->next = pcb; }
+    else { pcb->next = (*q)->next; (*q)->next = pcb; }
+}
+
+struct pcb *proc_dequeue(struct pcb **q) {
+    struct pcb *pcb = (*q)->next;
+    if (pcb == pcb->next) *q = 0;
+    else (*q)->next = pcb->next;
+    pcb->next = 0;
+    return pcb;
+}
---
+++ code/chapter3n/shared/syscall.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdint.h>
+
+enum syscall {
+    SYS_EXIT,       // exit process
+    SYS_SPAWN,      // start a new process
+    SYS_GET,        // read a character from the keyboard
+    SYS_PUT,        // write a character to the screen
+    SYS_CREATE,     // create a new file
+    SYS_READ,       // read a file
+    SYS_WRITE,      // write a file
+    SYS_SIZE,       // get the size of a file
+    SYS_DELETE      // delete a file
+};
+
+// The remaining stuff is for output
+
+enum ansi_color { ANSI_BLACK, ANSI_RED, ANSI_GREEN, ANSI_YELLOW,
+                     ANSI_BLUE,ANSI_MAGENTA, ANSI_CYAN, ANSI_WHITE };
+
+typedef uint16_t cell_t;
+
+#define CELL_FOREGROUND      8  // foreground color shift
+#define CELL_BACKGROUND     11  // background color shift
+#define CELL_ATTRIBUTES     14  // 2 bits for attrs (blinking, bold?)
+
+#define CELL(ch, fg, bg)     ((cell_t) (((ch) & 0xFF) | \
+                    (((fg) & 0x7) << CELL_FOREGROUND) | \
+                    (((bg) & 0x7) << CELL_BACKGROUND)))
+
+#define CELL_CH(c)          (c & 0xFF)
+#define CELL_FG(c)          (((c) >> CELL_FOREGROUND) & 0x7)
+#define CELL_BG(c)          (((c) >> CELL_BACKGROUND) & 0x7)
