a83a0e8567b3573608940dae3cf610268c41d59e
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-27 09:46:21 -0400

    Latest

--- code/chapter6n/apps/syslib.h
+++ code/chapter6n/apps/syslib.h
@@ -89,26 +89,35 @@ static inline void user_delete(int file) {
 #include "sched.h"
 #include "uart.h"
 #include "kprintf.h"
+#include "interrupt.h"
 
 static inline void user_exit() {
+    intr_disable();
     struct pcb *self = run_queue[proc_current]->next;
     proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
     kprintf("process ended<");
     proc_exit();
+    intr_enable();      // should never be called
 }
 
 static inline void user_spawn(int app, int row, char col, int wd, int ht, const void *args, int sz) {
+    intr_disable();
     sched_run(app, (struct rect){ row, col, wd, ht }, 0, 0);
+    intr_enable();
 }
 
 static inline void user_put(int row, int col, cell_t cell) {
+    intr_disable();
     struct pcb *self = run_queue[proc_current]->next;
     proc_put(self, row, col, cell);
+    intr_enable();
 }
 
 static inline int user_get(int row, int col, cell_t cursor_f, cell_t cursor_u) {
+    intr_disable();
     struct pcb *self = run_queue[proc_current]->next;
-    return uart_get(self, row, col, cursor_f, cursor_u);
+    int c = uart_get(self, row, col, cursor_f, cursor_u);
+    intr_enable();
 }
 
 #endif
