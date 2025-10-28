c7cc95742e813ab970e3d21c53289dbb1e71fbf8
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-28 00:01:09 -0400

    Latest

--- code/chapter8/start.s
+++ code/chapter8/start.s
@@ -4,7 +4,7 @@
 _start:
     csrr a0, mhartid      # read core ID into a0
     beq a0, x0, _end      # if hart 0, jump to _end and spin
-    li sp, 0x80400000     # set the stack pointer to top of stack
+    la sp, stack_end      # set sp to top of stack region
     call main             # call C main() code
 _end:
     wfi                   # go to sleep
