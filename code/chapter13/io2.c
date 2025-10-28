2f36d47a9d74424ccee45d8e972f7bee911fdb88
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-28 19:49:29 -0400

    Latest

--- code/chapter13/io2.c
+++ code/chapter13/io2.c
@@ -2,15 +2,17 @@
 #include "sched.h"
 #include "io.h"
 
-extern struct pcb *io_focus, *io_wait;
-
-void io_putchar(char c) { uart_putchar(c); }
+void io_putchar(char c) {
+    uart_putchar(c);
+}
 
 void io_put(struct pcb *pcb, cell_t c) {
     proc_put(pcb, pcb->kbd_row, pcb->kbd_col, c);
 }
 
 int io_get(struct pcb *self, int row, int col, cell_t cf, cell_t cu) {
+    extern struct pcb *io_focus, *io_wait;
+
     while (self->kbd_size == 0) {
         if (!self->kbd_warm) {
             if (io_focus != 0 && io_focus->kbd_waiting)
