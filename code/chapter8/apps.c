9e4f402fc85c91f829b815a48e76116e108c6db4
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-28 23:56:30 -0400

    Latest

--- code/chapter8/apps.c
+++ code/chapter8/apps.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include "frame.h"
 #include "sched.h"
-#include "kprintf.h"
 #include "string.h"
 #include "interrupt.h"
-#include "app_info.h"
+#include "die.h"
 
+#include "app_info.h"
 __attribute__((noreturn))
 void enter_user(void *entry, uintptr_t gp_val,
                 uintptr_t user_sp, size_t arg_size, uintptr_t ksp);
@@ -16,21 +16,14 @@ void exec_user(void) {
 
     self->base = frame_alloc();
     self->stack = frame_alloc();
-    if (self->base == 0 || self->stack == 0) {
-        proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
-        kprintf("out of memory<");
-        proc_exit();
-    }
+    if (self->base == 0 || self->stack == 0) die("out of memory");
 
     uint32_t gp_offset;
     struct app_info *ai = &app_table[self->executable - 2];
     gp_offset = ai->gp;
+
     uint32_t size = ai->end - ai->start;
-    if (size > PAGE_SIZE) {
-        proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
-        kprintf("executable too large<");
-        proc_exit();
-    }
+    if (size > PAGE_SIZE) die("executable too large");
 
     memcpy(self->base, ai->start, size);
     memset(&self->base[size], 0, PAGE_SIZE - size);
