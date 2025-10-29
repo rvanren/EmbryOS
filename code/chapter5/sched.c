b22160ff7b1f626b4429b853f5b81b2bd9907169
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-29 13:49:55 -0400

    Latest

--- code/chapter5/sched.c
+++ code/chapter5/sched.c
@@ -4,17 +4,13 @@
 #include "process.h"
 #include "interrupt.h"
 
-#define N_PRIORITIES 3
-
-static int current_priority;
-static struct pcb *run_queue[N_PRIORITIES];
-static struct pcb *zombies;   // list of PCBs pending free
+static struct pcb *run_queue, *zombies;
 
 void sched_init(struct pcb *first) {
-    proc_enqueue(&run_queue[0], first);
+    proc_enqueue(&run_queue, first);
 }
 
-struct pcb *sched_self() { return run_queue[current_priority]->next; }
+struct pcb *sched_self() { return run_queue->next; }
 
 static void reap_zombies(void) {
     while (zombies != 0) {
@@ -25,47 +21,35 @@ static void reap_zombies(void) {
 }
 
 void sched_block(struct pcb *current) {
-    current_priority = 0;
-    while (current_priority < N_PRIORITIES && run_queue[current_priority] == 0)
-        current_priority++;
-    struct pcb *next = run_queue[current_priority]->next;
+    struct pcb *next = run_queue->next;
     if (next != current) ctx_switch(&current->sp, next->sp);
     reap_zombies();
 }
 
 void sched_yield(void) {
-    if (current_priority == 0) {
-        struct pcb *me = proc_dequeue(&run_queue[0]);
-        proc_enqueue(&run_queue[1], me);
-        current_priority = 1;
-    }
-    struct pcb *current = run_queue[current_priority]->next;
-    run_queue[current_priority] = current;
+    struct pcb *current = run_queue->next;
+    run_queue = current;
     sched_block(current);
 }
 
 void sched_run(int executable, struct rect area, void *args, int size) {
     struct pcb *current = sched_self();
     struct pcb *pcb = proc_create(executable, area, args, size);
-    proc_enqueue(&run_queue[0], pcb);
-    current_priority = 0;
+    proc_enqueue(&run_queue, pcb);
     ctx_start(&current->sp, (char *) pcb + PAGE_SIZE);
     reap_zombies();
 }
 
+void sched_exit(void) {
+    struct pcb *pcb = proc_dequeue(&run_queue);
+    pcb->next = zombies;
+    zombies = pcb;
+    sched_block(pcb);
+}
+
 void sched_idle() {
-    struct pcb *pcb = sched_self();
-    proc_enqueue(&run_queue[2], pcb);
-    current_priority = 2;
     for (;;) {
         sched_yield();
         intr_enable(); __asm__ volatile ("wfi"); intr_disable();
     }
-}
-
-void sched_exit(void) {
-    struct pcb *pcb = sched_self();
-    pcb->next = zombies;
-    zombies = pcb;
-    sched_block(pcb);
 }
