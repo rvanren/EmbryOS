c8753a14d1c4c100ce4008baa0f3ec3e478e39e4
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-28 20:55:51 -0400

    Latest

--- code/chapter10/platform.h
+++ code/chapter10/platform.h
@@ -2,15 +2,34 @@
 
 // Platform-dependent constants go here
 
+#ifdef SIFIVE
+
 #define UART_BASE       0x10010000UL
+#define UART_SIFIVE
 #define UART_IRQ        4
 
 #define PLIC_BASE       0x0C000000UL
 #define CLINT_BASE      0x02000000UL
 
-#define HART_ID         1   // hart ID of the core that runs the kernel
-#define HART_CTX        1   // its S-mode context
-
-#define DELAY_MS        326087	// 1 ms of delay (see syscall.h)
+#define HART_CTX        1
 
 // #define NO_PMP       // define if no PMP
+
+#endif
+
+#ifdef VIRT
+
+#define UART_BASE       0x10000000UL
+#define UART_16550
+#define UART_IRQ        10
+
+#define PLIC_BASE       0x0C000000UL
+#define CLINT_BASE      0x02000000UL
+
+#define HART_CTX        2
+
+// #define NO_PMP       // define if no PMP
+
+#endif
+
+#define DELAY_MS        326087	// 1 ms of delay (see syscall.h)
