8f9380218771927457906f6d806366f873f9e154
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-29 13:40:51 -0400

    Latest

--- code/chapter5/platform.h
+++ code/chapter5/platform.h
@@ -6,6 +6,14 @@
 
 #define UART_BASE       0x10010000UL
 #define UART_SIFIVE
+#define UART_IRQ        4
+
+#define PLIC_BASE       0x0C000000UL
+#define CLINT_BASE      0x02000000UL
+
+#define HART_CTX        1
+
+// #define NO_PMP       // define if no PMP
 
 #endif
 
@@ -13,6 +21,12 @@
 
 #define UART_BASE       0x10000000UL
 #define UART_16550
+#define UART_IRQ        10
+
+#define PLIC_BASE       0x0C000000UL
+#define CLINT_BASE      0x02000000UL
+
+#define HART_CTX        2
 
 // #define NO_PMP       // define if no PMP
 
