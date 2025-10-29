4b2aecd7305c47a252cb906941abf14a106972f0
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-28 23:30:38 -0400

    Latest

--- code/chapter9/kprintf.c
+++ code/chapter9/kprintf.c
@@ -1,8 +1,52 @@
 #include <stdarg.h>
 #include "kprintf.h"
-#include "uart.h"
+#include "io.h"
 
-#define PRINTF kprintf
-#define PUTCHAR(c) uart_putchar(c)
+static void print_unsigned(unsigned int x, unsigned int base) {
+    char buf[16];
+    int i = 0;
+    do {
+        int digit = x % base;
+        buf[i++] = (digit < 10) ? '0' + digit : 'a' + (digit - 10);
+        x /= base;
+    } while (x != 0);
+    while (--i >= 0) io_putchar(buf[i]);
+}
 
-#include "shared/printf_template.c"
+void kprintf(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    for (; *fmt; fmt++) {
+        if (*fmt != '%') { io_putchar(*fmt); continue; }
+        fmt++;
+        switch (*fmt) {
+        case 'd': { int x = va_arg(ap, int);
+            if (x < 0) { io_putchar('-'); x = -x; }
+            print_unsigned((unsigned int) x, 10);
+            break;
+        }
+        case 'u': { unsigned int x = va_arg(ap, unsigned int);
+            print_unsigned(x, 10);
+            break;
+        }
+        case 'x': { unsigned int x = va_arg(ap, unsigned int);
+            print_unsigned(x, 16);
+            break;
+        }
+        case 's': { char *s = va_arg(ap, char *);
+            while (*s) io_putchar(*s++);
+            break;
+        }
+        case 'c': { int c = va_arg(ap, int);
+            io_putchar(c);
+            break;
+        }
+        case '%':
+            io_putchar('%');
+            break;
+        default:
+            io_putchar('%'); io_putchar(*fmt);
+        }
+    }
+    va_end(ap);
+}
