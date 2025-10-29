e62e001b38bc8bc2eb9a6386c314835753cb180d
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-29 08:10:47 -0400

    Latest

--- code/chapter10/io2.c
+++ code/chapter10/io2.c
@@ -1,36 +1,47 @@
-#include "uart.h"
 #include "sched.h"
 #include "io.h"
 
-void io_putchar(char c) {
-    uart_putchar(c);
-}
+struct pcb *io_wait = 0;    // circular wait queue
+struct pcb *io_focus;       // keyboard focus
 
-void io_put(struct pcb *pcb, cell_t c) {
-    proc_put(pcb, pcb->kbd_row, pcb->kbd_col, c);
-}
-
-int io_get(struct pcb *self, int row, int col, cell_t cf, cell_t cu) {
-    extern struct pcb *io_focus, *io_wait;
-
-    while (self->kbd_size == 0) {
-        if (!self->kbd_warm) {
-            if (io_focus != 0 && io_focus->kbd_waiting)
-                io_put(io_focus, io_focus->cu);
-            io_focus = self;
-            self->kbd_warm = 1;
-        }
-        proc_put(self, row, col, self == io_focus ? cf : cu);
-        self->cf = cf; self->cu = cu;
-        self->kbd_waiting = 1;
-        self->kbd_row = row; self->kbd_col = col;
-        proc_dequeue(&run_queue[proc_current]);
-        if (io_wait == 0) io_wait = self->next = self;
-        else { self->next = io_wait->next; io_wait->next = self; }
-        sched_block(self);
+static void io_tab(void) {
+    if (io_wait == 0) { if (io_focus == 0) io_putchar(7); }
+    else if (io_focus == 0) {
+        io_focus = io_wait;
+        io_put(io_focus, io_focus->cf);
     }
-    char c = self->kbd_buf[self->kbd_tail];
-    self->kbd_tail = (self->kbd_tail + 1) % KBD_BUF_SIZE;
-    self->kbd_size--;
-    return c;
+    else {
+        io_put(io_focus, io_focus->cu);
+        if (io_focus == io_wait) io_wait = io_wait->next;
+        io_focus = io_wait;
+        io_put(io_focus, io_focus->cf);
+    }
+}
+
+static void io_char(char c) {
+    struct pcb *pcb = io_focus;
+    if (pcb->kbd_size >= KBD_BUF_SIZE) { io_putchar(7 /* beep */); return; }
+    if (pcb->kbd_waiting) {
+        pcb->kbd_waiting = 0;
+        if (pcb == pcb->next) io_wait = 0;
+        else {
+            struct pcb *prev = io_wait;
+            while (prev->next != pcb) prev = prev->next;
+            prev->next = pcb->next;
+            if (io_wait == pcb) io_wait = prev;
+        }
+        proc_enqueue(&run_queue[0], pcb);
+    }
+    pcb->kbd_buf[(pcb->kbd_tail + pcb->kbd_size) % KBD_BUF_SIZE] = c;
+    pcb->kbd_size++;
+}
+
+void io_received(char c) {
+    if (c == '\t') io_tab();
+    else if (io_focus != 0) io_char(c);
+    else io_putchar(7);    // beep
+}
+
+void io_exit(struct pcb *self) {
+    if (self == io_focus) io_focus = 0;
 }
