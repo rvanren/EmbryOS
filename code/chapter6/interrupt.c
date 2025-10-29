5904c587805a9d37ba3d4306ad3ebb830bcd667a
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-29 09:00:48 -0400

    Latest

--- code/chapter6/interrupt.c
+++ code/chapter6/interrupt.c
@@ -3,7 +3,6 @@
 #include "interrupt.h"
 #include "sched.h"
 #include "kprintf.h"
-#include "pmp.h"
 
 #define MIE_MASK (1u << 3)
 
@@ -35,9 +34,6 @@ void software_trap_handler(struct trap_frame *tf) {
             (*handlers[INTR_EXCEPTION])(tf);
         }
     }
-
-    struct pcb *self = run_queue[proc_current]->next;
-    if (self->base != 0) pmp_load(self);
 }
 
 void intr_set_handler(enum intr_class which, trap_entry_t handler) {
