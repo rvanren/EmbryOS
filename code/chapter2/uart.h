11082ece049829e12a5588856f03c24086c774f5
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-28 21:08:11 -0400

    Latest

--- code/chapter2/uart.h
+++ code/chapter2/uart.h
@@ -3,5 +3,6 @@
 // A UART (Universal Asynchronous Receiver Transmitter) is used for input/output to
 // simple serial peripherals (external devices) like a keyboard, mouse, or the screen.
 
-void uart_init(void);   // initialize this module
-void uart_putchar(char c);   // write a character to the screen
+void uart_putchar(char c);        // write c to the screen
+void uart_isr();                  // the UART interrupt service routine
+void uart_init(void);             // initialize this module
