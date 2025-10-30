1e4632752724fb45d95adfae8b55ce4339cca12d
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-30 19:45:22 -0400

    Latest

--- code/chapter6/hello.c
+++ code/chapter6/hello.c
@@ -1,39 +1,29 @@
 #include "sched.h"
 #include "kprintf.h"
 #include "syscall.h"
-#include "uart.h"
 #include "screen.h"
 #include "interrupt.h"
 #include "frame.h"
-#include "plic.h"
-
 #include "mtime.h"
+#include "sbi.h"
 
 #define QUANTUM          50000        // 50 milliseconds
 
 void timer_handler(struct trap_frame *tf) {
     sched_yield();
-    mtime_reset(QUANTUM); // add another quantum
-}
-
-void exception_handler(struct trap_frame *tf) {
-    struct pcb *self = sched_self();
-    proc_put(self, 0, 0, CELL('>', ANSI_BLACK, ANSI_RED));
-    kprintf("trap: cause=%d mepc=%x mtval=%x<",
-                        tf->mcause & 0xFFF, tf->mepc, tf->mtval);
-    sched_exit();
+    uint64_t now = mtime_get();
+    sbi_set_timer(now + QUANTUM);
 }
 
 int main(void) {
-    frame_init(); intr_init(); uart_init();
+    frame_init(); intr_init();
     intr_set_handler(INTR_EXCEPTION, exception_handler);
     plic_init();
     sched_init(proc_init((struct rect){ 0, 0, 80, 24 }));
-
     intr_set_handler(INTR_EXTERNAL, plic_handler);
-    mtime_init();
     intr_set_handler(INTR_TIMER, timer_handler);
-    mtime_reset(QUANTUM);
+    uint64_t now = mtime_get();
+    sbi_set_timer(now + QUANTUM);
 
     screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
                         CELL(' ', ANSI_WHITE, ANSI_BLACK));
