9dbe665bd7b17e4d0bfa0347486cbbdd4735b158
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-30 17:56:33 -0400

    Latest

--- code/chapter2/hello.c
+++ code/chapter2/hello.c
@@ -1,12 +1,6 @@
-#include "sched.h"
-#include "kprintf.h"
-#include "syscall.h"
-#include "uart.h"
 #include "screen.h"
 
 int main(void) {
-    uart_init();
-
     screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
                         CELL(' ', ANSI_WHITE, ANSI_BLACK));
 
