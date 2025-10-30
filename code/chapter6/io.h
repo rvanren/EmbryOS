f0e5a73f24430dad3608b79bb1c40919086fd067
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-30 19:47:04 -0400

    Latest

--- code/chapter6/io.h
+++ code/chapter6/io.h
@@ -1,1 +1,10 @@
+#pragma once
+
+#include "process.h"
+#include "syscall.h"
+
+void io_received(char c);       // somebody typed c
+void io_put(struct pcb *pcb, cell_t c);
+void io_exit(struct pcb *pcb);  // called when process exits
 void io_putchar(char c);        // write c to the screen
+int  io_get(struct pcb *self, int row, int col, cell_t cf, cell_t cu);
