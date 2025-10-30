fc3f73b2bfde3e7631b58d6fdbc06e9f64d633b1
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-30 17:17:33 -0400

    Latest

--- code/chapter1/hello.c
+++ code/chapter1/hello.c
@@ -1,10 +1,6 @@
-#include "sched.h"
 #include "kprintf.h"
-#include "syscall.h"
-#include "uart.h"
 
 int main(void) {
-    uart_init();
     for (;;) {
         kprintf("EmbryOS: Life Has Begun\n");
         user_delay(1000);
--- code/chapter1/platform.h
+++ code/chapter1/platform.h
@@ -2,18 +2,4 @@
 
 // Platform-dependent constants go here
 
-#ifdef SIFIVE
-
-#define UART_BASE       0x10010000UL
-#define UART_SIFIVE
-
-#endif
-
-#ifdef VIRT
-
-#define UART_BASE       0x10000000UL
-#define UART_16550
-
-#endif
-
 #define DELAY_MS        326087	// 1 ms of delay (see syscall.h)
