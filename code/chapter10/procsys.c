ccc4dfbf92e0f3dde0e2851e46babca303a17375
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-31 14:29:09 -0400

    Latest

--- code/chapter10/procsys.c
+++ code/chapter10/procsys.c
@@ -8,9 +8,9 @@ void proc_check_legal(struct pcb *self, uintptr_t start, int size) {
     uintptr_t end = start + size - 1;
     if (end < start) die("bad system call size");
     uintptr_t base_lo  = (uintptr_t) self->base;
-    uintptr_t base_hi  = base_lo  + PAGE_SIZE - 1;
+    uintptr_t base_hi  = base_lo  + FRAME_SIZE - 1;
     uintptr_t stack_lo = (uintptr_t) self->stack;
-    uintptr_t stack_hi = stack_lo + PAGE_SIZE - 1;
+    uintptr_t stack_hi = stack_lo + FRAME_SIZE - 1;
     if (!((start >= base_lo && end <= base_hi) ||
                     (start >= stack_lo && end <= stack_hi)))
         die("bad system call address");
