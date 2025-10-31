b9c9e5c5a7d2ba866f9362104f60db0cfe27dc79
Robbert van Renesse <rvr@cs.cornell.edu>
2025-10-30 20:42:12 -0400

    Latest

--- code/chapter6/platform.h
+++ code/chapter6/platform.h
@@ -5,9 +5,21 @@
 #define PAGE_SHIFT      13
 #define PAGE_SIZE       (1 << PAGE_SHIFT)
 
+#ifdef SIFIVE
+
+#define UART_SIFIVE
+#define UART_BASE       0x10010000UL
+#define UART_IRQ        4
+
+#endif
+
+#ifdef VIRT
+
 #define UART_16550
 #define UART_BASE       0x10000000UL
 #define UART_IRQ        10
+
+#endif
 
 #define PLIC_BASE       0x0C000000UL
 
