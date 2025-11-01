61049b316f22e80ed620699248b3810fbfbb85ad
Robbert van Renesse <rvr@cs.cornell.edu>
2025-11-01 15:34:19 -0400

    Latest

--- code/chapter3/platform.h
+++ code/chapter3/platform.h
@@ -12,4 +12,7 @@ typedef uint32_t uword_t;   // unsigned machine word
 
 // Platform-dependent constants go here
 
+#define PAGE_SHIFT      13
+#define PAGE_SIZE       (1 << PAGE_SHIFT)
+
 #define DELAY_MS        326087	// 1 ms of delay (see syscall.h)
