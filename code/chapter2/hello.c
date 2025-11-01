b17361aeeb107bc6da1c91f8e7a641f1e5ea22bf
Robbert van Renesse <rvr@cs.cornell.edu>
2025-11-01 14:58:30 -0400

    Latest

--- code/chapter2/hello.c
+++ code/chapter2/hello.c
@@ -1,11 +1,8 @@
 #include "kprintf.h"
 #include "syscall.h"
 #include "screen.h"
-#include "frame.h"
 
 int main(void) {
-    frame_init();
-
     screen_fill(0, 0, SCREEN_COLS, SCREEN_ROWS,
                         CELL(' ', ANSI_WHITE, ANSI_BLACK));
 
