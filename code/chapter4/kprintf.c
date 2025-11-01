01facaa063e8fd0ee80e4425726bd52b9edcaa4f
Robbert van Renesse <rvr@cs.cornell.edu>
2025-11-01 15:45:33 -0400

    Latest

--- code/chapter4/kprintf.c
+++ code/chapter4/kprintf.c
@@ -1,51 +1,49 @@
 #include <stdarg.h>
 #include "kprintf.h"
 #include "io.h"
+#include "platform.h"
 
-static void print_unsigned(unsigned int x, unsigned int base) {
-    char buf[16];
+static void print_unsigned(uword_t x, unsigned int base) {
+    char buf[32];
     int i = 0;
     do {
-        int digit = x % base;
-        buf[i++] = (digit < 10) ? '0' + digit : 'a' + (digit - 10);
+        int d = x % base;
+        buf[i++] = (d < 10) ? '0'+d : 'a'+(d-10);
         x /= base;
-    } while (x != 0);
+    } while (x);
     while (--i >= 0) io_putchar(buf[i]);
 }
 
 void kprintf(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
+    va_list ap; va_start(ap, fmt);
     for (; *fmt; fmt++) {
         if (*fmt != '%') { io_putchar(*fmt); continue; }
         fmt++;
         switch (*fmt) {
-        case 'd': { int x = va_arg(ap, int);
+        case 'd': {
+            int x = va_arg(ap, int);
             if (x < 0) { io_putchar('-'); x = -x; }
-            print_unsigned((unsigned int) x, 10);
+            print_unsigned((uword_t) x, 10);
             break;
         }
-        case 'u': { unsigned int x = va_arg(ap, unsigned int);
-            print_unsigned(x, 10);
+        case 'D': {
+            sword_t x = va_arg(ap, sword_t);
+            if (x < 0) { io_putchar('-'); x = -x; }
+            print_unsigned((uword_t) x, 10);
             break;
         }
-        case 'x': { unsigned int x = va_arg(ap, unsigned int);
-            print_unsigned(x, 16);
-            break;
-        }
-        case 's': { char *s = va_arg(ap, char *);
+        case 'u': print_unsigned(va_arg(ap, unsigned int), 10); break;
+        case 'U': print_unsigned(va_arg(ap, uword_t),      10); break;
+        case 'x': print_unsigned(va_arg(ap, unsigned int), 16); break;
+        case 'X': print_unsigned(va_arg(ap, uword_t),      16); break;
+        case 'c': io_putchar(va_arg(ap, int)); break;
+        case 's': {
+            char *s = va_arg(ap, char*);
             while (*s) io_putchar(*s++);
             break;
         }
-        case 'c': { int c = va_arg(ap, int);
-            io_putchar(c);
-            break;
-        }
-        case '%':
-            io_putchar('%');
-            break;
-        default:
-            io_putchar('%'); io_putchar(*fmt);
+        case '%': io_putchar('%'); break;
+        default:  io_putchar('%'); io_putchar(*fmt);
         }
     }
     va_end(ap);
