31376c9904009263c2f72f79aecc183d716baaa3
Robbert van Renesse <rvr@cs.cornell.edu>
2025-12-27 16:09:33 -0500

    Improved comment

--- chapter09/bd.h
+++ chapter09/bd.h
@@ -11,7 +11,7 @@ struct block { char bytes[BLOCK_SIZE]; };
 struct bd {
     void *state;     // depends on implementation of this interface
     int  (*alloc)(void *state);                 // allocate an inode
-    int  (*size)(void *state, int inode);       // size in blocks
+    int  (*size)(void *state, int inode);       // maximum size in blocks
     void (*read)(void *state, int inode, int blk, void *dst);
     void (*write)(void *state, int inode, int blk, const void *src);
     void (*free)(void *state, int inode);       // free an inode
